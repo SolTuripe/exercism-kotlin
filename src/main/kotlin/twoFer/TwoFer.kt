3f871824594c908cb454d474933b73fc9c98d2d6
Sol Turipe <solcis.bio@gmail.com>
2022-09-13 11:05:01 +0200

    two fer interaction done

---
+++ src/main/kotlin/twoFer/TwoFer.kt
@@ -0,0 +1,7 @@
+package twoFer
+
+class TwoFer() {
+    internal fun twoFer(name: String = "you"): String {
+        return "One for $name, one for me."
+    }
+}
---
+++ src/test/kotlin/twoFer/TwoFerTest.kt
@@ -0,0 +1,29 @@
+package twoFer
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class TwoFerTest {
+
+  private val twoFER = TwoFer()
+
+    @Test
+    fun `no name given`() {
+        assertEquals("One for you, one for me.", twoFER.twoFer())
+    }
+
+    @Test
+    fun `a name given`() {
+        assertEquals("One for Alice, one for me.", twoFER.twoFer("Alice"))
+    }
+
+    @Test
+    fun `another name given`() {
+        assertEquals("One for Bob, one for me.", twoFER.twoFer("Bob"))
+    }
+
+    @Test
+    fun `empty string given`() {
+        assertEquals("One for , one for me.", twoFER.twoFer(""))
+    }
+}
