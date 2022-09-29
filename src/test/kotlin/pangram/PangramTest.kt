61fcc9bf2e3a931495bb49f4f5741fe34c08d192
Sol Turipe <solcis.bio@gmail.com>
2022-09-29 11:58:05 +0200

    perfect lower case test

--- src/test/kotlin/pangram/PangramTest.kt
+++ src/test/kotlin/pangram/PangramTest.kt
@@ -2,11 +2,17 @@ package pangram
 
 import org.junit.jupiter.api.Test
 import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class PangramTest {
 
     @Test
     fun `empty sentence`() {
         assertFalse(Pangram.isPangram(""))
+    }
+
+    @Test
+    fun `perfect lower case`() {
+        assertTrue(Pangram.isPangram("abcdefghijklmnopqrstuvwxyz"))
     }
 }
